72d675570e18742b6e2521cf27a921c4154aeac0
memray <memray0@gmail.com>
2019-05-13 02:02:23 -0700

    eval: add unique_pred_num, dup_pred_num, f_score_hard, precision_hard in final report

--- script/srun_one2many/kpeval-beam50-maxlen40/kpeval_kp20k_valid500.sh
+++ script/srun_one2many/kpeval-beam50-maxlen40/kpeval_kp20k_valid500.sh
@@ -11,4 +11,4 @@
 #SBATCH --qos=long
 
 # Run the job
-python kp_run_eval.py -config script/srun_one2many/kpeval/config-test-keyphrase-one2many.yml -data_dir data/keyphrase/meng17/ -ckpt_dir models/keyphrase/meng17/ -output_dir output/keyphrase/meng17-one2many-beam50-maxlen40/ -gpu -1 -testsets kp20k_valid500
+python kp_run_eval.py -config script/srun_one2many/kpeval-beam50-maxlen40/config-test-keyphrase-one2many.yml -data_dir data/keyphrase/meng17/ -ckpt_dir models/keyphrase/meng17/ -output_dir output/keyphrase/meng17-one2many-beam50-maxlen40/ -gpu -1 -testsets kp20k_valid500
